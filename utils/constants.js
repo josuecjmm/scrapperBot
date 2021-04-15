ea01438c3e0704f386bb54f8a90f66ef7fbfc91f
v-jomartinez <v-jomartinez@youngliving.com>
2021-04-15 15:33:58 -0600

    Adding techzilla scrapper

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,7 @@
 exports.urls = {
     extremeTech: 'https://extremetechcr.com/tienda/18-tarjetas-de-video',
-    intelec: 'https://www.intelec.co.cr/pcgaming/gpu'
+    intelec: 'https://www.intelec.co.cr/pcgaming/gpu',
+    techzilla: 'https://www.techzilla.cr/?search_id=default&post_type=product&filters=product_cat[303]'
 }
 
 exports.possibleCards = ['3060ti', 'ti', '3070', '3080', '5700']
