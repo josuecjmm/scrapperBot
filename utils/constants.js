af29d0b7de7eacf78abc479253cff3d1556c13de
Camilo Martinez Mora <pablo_cmm@hotmail.com>
2021-04-17 17:46:52 -0600

    adding pctodocr to the bot

--- utils/constants.js
+++ utils/constants.js
@@ -3,6 +3,7 @@ exports.urls = {
     intelec: 'https://www.intelec.co.cr/pcgaming/gpu',
     techzilla: 'https://www.techzilla.cr/?search_id=default&post_type=product&filters=product_cat[303]',
     ticotek: 'https://www.ticotek.com/categoria-producto/componentes-pc/tarjetas-de-video/',
+    pctodocr: 'https://pctodocr.com/product-category/componentes/tarjeta-video'
 }
 
-exports.possibleCards = ['3060ti', 'ti', '3070', '3080', '5700']
+exports.possibleCards = ['3060ti', 'ti', '3070', '3080', '5700', '1660']
